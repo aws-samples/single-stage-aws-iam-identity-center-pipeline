50310a19c35fb1cfc8ea2beb5387d07e1dd91e13
Benjamin Morris <sirromb@amazon.com>
2024-05-31 13:47:16 -0700

    fix f-string usage of join

--- terraform/bootstrap/migrate_assignments_to_delegated_admin_model.py
+++ terraform/bootstrap/migrate_assignments_to_delegated_admin_model.py
@@ -341,9 +341,7 @@ def main(read_only, region):
     print(
         f"[ACTION REQUIRED] Assignments have been migrated for the following permission sets, but YOU WILL NEED TO MANUALLY REMOVE THESE PERMISSION SETS FROM THE LIST OF PERMISSION SETS PROVISIONED TO THE MANAGEMENT ACCOUNT."
     )
-    list_of_permission_sets_to_unprovision = "\n".join(
-        migrated_permission_set_names
-    )
+    list_of_permission_sets_to_unprovision = "\n".join(migrated_permission_set_names)
     print(
         f"[ACTION REQUIRED] Permission Sets to unprovision from the management account using the AWS Console:\n {list_of_permission_sets_to_unprovision}"
     )
@@ -457,9 +455,11 @@ def main(read_only, region):
     print(
         f"[ACTION REQUIRED] Assignments have been migrated for the following permission sets, but YOU WILL NEED TO MANUALLY REMOVE THESE PERMISSION SETS FROM THE LIST OF PERMISSION SETS PROVISIONED TO THE MANAGEMENT ACCOUNT."
     )
+    list_of_permission_sets_to_unprovision = "\n".join(migrated_permission_set_names)
     print(
-        f"[ACTION REQUIRED] Permission Sets to unprovision from the management account using the AWS Console:\n {"\n".join(migrated_permission_set_names)}"
+        f"[ACTION REQUIRED] Permission Sets to unprovision from the management account using the AWS Console:\n {list_of_permission_sets_to_unprovision}"
     )
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
